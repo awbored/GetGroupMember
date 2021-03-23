45e11a501942d190d418ebf3889fe0f5d8daf769
Andrew Wayne <79549187+awbored@users.noreply.github.com>
2021-03-23 11:46:34 -0700

    Add files via upload

---
+++ GetGroupMember.ps1
@@ -0,0 +1,12 @@
+﻿$GroupName = $args[0]
+IF($GroupName -eq $null){$GroupName = Read-Host -Prompt "What's the group name?"}
+$Attrib = Get-Content .\attrib.txt
+
+Get-ADGroupMember -Identity $GroupName -Recursive | 
+    Where-Object objectClass -eq 'user' |
+    Get-ADUser -Properties $Attrib | 
+    Select-Object $Attrib | 
+    Sort sAMAccountName | 
+Export-CSV ".\$GroupName.csv" -NoTypeInformation -Encoding UTF8
+
+explorer ".\$GroupName.csv"
